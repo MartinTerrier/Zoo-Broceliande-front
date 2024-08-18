12fb90c1c1a331c8202e7b4fe54adca7b8dfec84
Martin Terrier <martin.terrier@arkyan.com>
2024-08-18 14:20:33 +0200

    Create service modal WIP.

--- js/services.js
+++ js/services.js
@@ -1,7 +1,23 @@
 import { apiBaseUrl, getConnexionToken, manageRoleElements } from "./script.js";
 
 const serviceList = document.getElementById('service-list');
-const modalContainer = document.getElementById('modal-container');
+const serviceUpdateModalEl = document.getElementById(`edit-service-modal`);
+const serviceUpdateModal = new bootstrap.Modal(serviceUpdateModalEl);
+const serviceDeleteModalEl = document.getElementById('delete-service-modal');
+const serviceDeleteModal = new bootstrap.Modal(serviceDeleteModalEl);
+const serviceCreateModalEl = document.getElementById('create-service-modal');
+const serviceCreateModal = new bootstrap.Modal(serviceCreateModalEl);
+const serviceUpdateForm = document.getElementById(`service-update-form`);
+const serviceUpdateNameInput = document.getElementById('service-update-name-input');
+const serviceUpdateContentInput = document.getElementById('service-update-content-input');
+const serviceUpdateButton = document.getElementById(`service-update-button`);
+const serviceDeleteWarning = document.getElementById('delete-service-modal-warning');
+const serviceDeleteButton = document.getElementById('delete-service-button');
+const serviceCreateForm = document.getElementById('service-create-form');
+const serviceCreateNameInput = document.getElementById('service-create-name-input');
+const serviceCreateContentInput = document.getElementById('service-create-content-input');
+const serviceCreateButton = document.getElementById('service-create-button');
+let serviceToUpdate;
 
 const requestOptions = {
   method: "GET",
@@ -15,14 +31,13 @@ const servicesArray = await fetch(`${apiBaseUrl}/services`, requestOptions)
 async function displayAllServices () {
 
   let serviceHtml = '';
-  let modalHtml = '';
 
   servicesArray.forEach((service) => {
     const even = (service.id % 2 === 0);
     serviceHtml += `<article ${even ? '': 'class="bg-dark text-white"'}>
         <div class="action-image-buttons">
-          <button type="button" id="edit-service-button-${service.id}" class="btn btn-outline-${even ? 'dark': 'secondary'} me-3" data-show="employee" data-bs-toggle="modal" data-bs-target="#edit-service-modal-${service.id}"><i class="bi bi-pencil-square"></i></button>
-          <button type="button" id="delete-service-button-${service.id}" class="btn btn-outline-${even ? 'dark': 'secondary'} me-3" data-show="admin" data-bs-toggle="modal" data-bs-target="#delete-service-modal-${service.id}"><i class="bi bi-trash"></i></button>
+          <button type="button" id="edit-service-button-${service.id}" class="btn btn-outline-${even ? 'dark': 'secondary'} me-3" data-show="employee" data-bs-toggle="modal" data-bs-target="#edit-service-modal" data-bs-service-id="${service.id}"><i class="bi bi-pencil-square"></i></button>
+          <button type="button" id="delete-service-button-${service.id}" class="btn btn-outline-${even ? 'dark': 'secondary'} me-3" data-show="admin" data-bs-toggle="modal" data-bs-target="#delete-service-modal" data-bs-service-id="${service.id}"><i class="bi bi-trash"></i></button>
         </div>
         <div class="container p-4">
             <h2 id = 'service-name-${service.id}' class="text-center text-primary">${service.name}</h2>
@@ -36,49 +51,26 @@ async function displayAllServices () {
             </div>
         </div>
     </article>`;
-
-    modalHtml += `<div class="modal fade" id="edit-service-modal-${service.id}" tabindex="-1" aria-labelledby="edit-service-modal-${service.id}-label" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h2 class="modal-title fs-5" id="edit-service-modal-${service.id}-label">Edition du service</h2>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form id='service-${service.id}-update-form'>
-                    <div class="mb-3">
-                      <label for="service-${service.id}-name-input" class="form-label">Nom</label>
-                      <input type="text" class="form-control" id="service-${service.id}-name-input" value="${service.name}" name="name"> 
-                    </div>
-                    <div class="mb-3">
-                      <label for="service-${service.id}-content-input" class="form-label">Description</label>
-                      <textarea class="form-control" id="service-${service.id}-content-input"  name="description">${service.description}</textarea>
-                    </div>
-                    <div class="text-center">
-                        <button type="button" id='service-${service.id}-update-button' class="btn btn-primary text-white">Enregistrer</button>
-                    </div>
-                </form>
-            </div>
-          </div>
-        </div>
-      </div>`;
   });
 
   serviceList.innerHTML = serviceHtml;
-  modalContainer.innerHTML = modalHtml;
   manageRoleElements();
 }
 
 displayAllServices();
 
-servicesArray.forEach((service) => {
-  const serviceUpdateModal = new bootstrap.Modal(document.getElementById(`edit-service-modal-${service.id}`));
-  const serviceName = document.getElementById(`service-name-${service.id}`);
-  const serviceDescription = document.getElementById(`service-description-${service.id}`);
-  const serviceUpdateForm = document.getElementById(`service-${service.id}-update-form`);
-  const serviceUpdateButton = document.getElementById(`service-${service.id}-update-button`);
-    
-  const updateService = async () => {
+serviceUpdateModalEl.addEventListener('show.bs.modal', (event) => {
+  const button = event.relatedTarget;
+  const serviceId = button.getAttribute('data-bs-service-id');
+  serviceToUpdate = servicesArray.find((service) => (service.id === +serviceId));
+  serviceUpdateNameInput.value = serviceToUpdate.name;
+  serviceUpdateContentInput.innerHTML = serviceToUpdate.description;
+  serviceUpdateButton.addEventListener('click', updateService);
+});
+
+const updateService = async () => {
+    const serviceName = document.getElementById(`service-name-${serviceToUpdate.id}`);
+    const serviceDescription = document.getElementById(`service-description-${serviceToUpdate.id}`);
     const formData = new FormData(serviceUpdateForm);
     const formDataName = formData.get('name');
     const formDataDescription = formData.get('description');
@@ -99,18 +91,61 @@ servicesArray.forEach((service) => {
       redirect: 'follow'
     };
 
-    await fetch(`${apiBaseUrl}/services/${service.id}`, requestOptions)
+    await fetch(`${apiBaseUrl}/services/${serviceToUpdate.id}`, requestOptions)
     .then((response) => {
       if (response.ok) {
         serviceName.innerHTML = formDataName;
         serviceDescription.innerHTML = formDataDescription;
         serviceUpdateModal.hide();
       } else {
-        alert("Vos modifiations n'ont pas pu être sauvegardées.");
+        alert("Vos modifications n'ont pas pu être sauvegardées.");
       }
     })
     .catch((error) => console.error(error));
-  }
+}
 
-  serviceUpdateButton.addEventListener('click', updateService);
-});
+serviceDeleteModalEl.addEventListener('show.bs.modal', (event) => {
+  const button = event.relatedTarget;
+  const serviceId = button.getAttribute('data-bs-service-id');
+  const serviceToUpdate = servicesArray.find((service) => (service.id === Number(serviceId)));
+  serviceDeleteWarning.innerHTML = `Vous vous apprêtez à supprimer le service "${serviceToUpdate.name}".`
+  serviceDeleteButton.addEventListener('click', deleteService);
+})
+
+const deleteService = async () => {}
+
+const createService = async () => {
+  const formData = new FormData(serviceCreateForm);
+  const formDataName = formData.get('name');
+  const formDataDescription = formData.get('description');
+  const token = getConnexionToken();
+
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+   myHeaders.append('Authorization', `Bearer ${token}`);
+
+  const urlEncoded = new URLSearchParams();
+  urlEncoded.append('name', formDataName);
+  urlEncoded.append('description', formDataDescription);
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: urlEncoded,
+    redirect: 'follow'
+  };
+
+  await fetch(`${apiBaseUrl}/services`, requestOptions)
+    .then((response) => {
+      if (response.ok) {
+        servicesArray.push(response);
+        displayAllServices();
+        serviceCreateModal.hide();
+      } else {
+        alert("Le nouveau service n'a pas pu être créé.");
+      }
+    })
+    .catch((error) => console.error(error));
+}
+
+serviceCreateButton.addEventListener("click", createService);
