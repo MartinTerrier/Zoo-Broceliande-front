34159c637715112eba390affdf3600f0dfa3f2f2
Martin Terrier <martin.terrier@gmail.com>
2024-10-06 23:36:51 +0200

    Services page complete with CRUD and image management.

--- js/services.js
+++ js/services.js
@@ -1,30 +1,24 @@
 import { apiBaseUrl, getConnexionToken, manageRoleElements } from "./script.js";
 
 const serviceList = document.getElementById('service-list');
-const serviceUpdateModalEl = document.getElementById(`edit-service-modal`);
+const serviceUpdateModalEl = document.getElementById('edit-service-modal');
 const serviceUpdateModal = new bootstrap.Modal(serviceUpdateModalEl);
 const serviceDeleteModalEl = document.getElementById('delete-service-modal');
 const serviceDeleteModal = new bootstrap.Modal(serviceDeleteModalEl);
 const serviceCreateModalEl = document.getElementById('create-service-modal');
 const serviceCreateModal = new bootstrap.Modal(serviceCreateModalEl);
-const serviceUpdateForm = document.getElementById(`service-update-form`);
+const serviceUpdateForm = document.getElementById('service-update-form');
 const serviceUpdateNameInput = document.getElementById('service-update-name-input');
 const serviceUpdateContentInput = document.getElementById('service-update-content-input');
-const serviceUpdateButton = document.getElementById(`service-update-button`);
+const serviceUpdateButton = document.getElementById('service-update-button');
 const serviceDeleteWarning = document.getElementById('delete-service-modal-warning');
 const serviceDeleteButton = document.getElementById('delete-service-button');
 const serviceCreateForm = document.getElementById('service-create-form');
-const serviceCreateNameInput = document.getElementById('service-create-name-input');
-const serviceCreateContentInput = document.getElementById('service-create-content-input');
 const serviceCreateButton = document.getElementById('service-create-button');
 let serviceToUpdate;
+let serviceToDelete;
 
-const requestOptions = {
-  method: "GET",
-  redirect: "follow"
-};
-
-const servicesArray = await fetch(`${apiBaseUrl}/services`, requestOptions)
+const servicesArray = await fetch(`${apiBaseUrl}/services`, { method: "GET", redirect: "follow" })
   .then((response) => response.json())
   .catch((error) => console.error(error));
 
@@ -32,8 +26,9 @@ async function displayAllServices () {
 
   let serviceHtml = '';
 
-  servicesArray.forEach((service) => {
-    const even = (service.id % 2 === 0);
+  servicesArray.forEach((service, index) => {
+    const even = (index % 2 === 0);
+
     serviceHtml += `<article ${even ? '': 'class="bg-dark text-white"'}>
         <div class="action-image-buttons">
           <button type="button" id="edit-service-button-${service.id}" class="btn btn-outline-${even ? 'dark': 'secondary'} me-3" data-show="employee" data-bs-toggle="modal" data-bs-target="#edit-service-modal" data-bs-service-id="${service.id}"><i class="bi bi-pencil-square"></i></button>
@@ -43,10 +38,10 @@ async function displayAllServices () {
             <h2 id = 'service-name-${service.id}' class="text-center text-primary">${service.name}</h2>
             <div class="row row-cols-2 align-items-center">
                 <div class="col">
-                    ${even ? `<p id='service-description-${service.id}'>${service.description}</p>` : `<img src="../images/services-${service.id}-large.jpg" alt="une image trop cool" class="w-100 rounded"></img>`}
+                    ${even ? `<p id='service-description-${service.id}'>${service.description}</p>` : `<img id="service-image-${service.id}" src="${apiBaseUrl}/services/image/${service.id}" alt="une image trop cool" class="w-100 rounded"></img>`}
                 </div>
                 <div class="col">
-                    ${!even ? `<p id='service-description-${service.id}'>${service.description}</p>` : `<img src="../images/services-${service.id}-large.jpg" alt="une image trop cool" class="w-100 rounded"></img>`}
+                    ${!even ? `<p id='service-description-${service.id}'>${service.description}</p>` : `<img id="service-image-${service.id}" src="${apiBaseUrl}/services/image/${service.id}" alt="une image trop cool" class="w-100 rounded"></img>`}
                 </div>
             </div>
         </div>
@@ -69,33 +64,28 @@ serviceUpdateModalEl.addEventListener('show.bs.modal', (event) => {
 });
 
 const updateService = async () => {
-    const serviceName = document.getElementById(`service-name-${serviceToUpdate.id}`);
-    const serviceDescription = document.getElementById(`service-description-${serviceToUpdate.id}`);
     const formData = new FormData(serviceUpdateForm);
-    const formDataName = formData.get('name');
-    const formDataDescription = formData.get('description');
     const token = getConnexionToken();
     
     const myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
     myHeaders.append('Authorization', `Bearer ${token}`);
 
-    const urlEncoded = new URLSearchParams();
-    urlEncoded.append('name', formDataName);
-    urlEncoded.append('description', formDataDescription);
-
     const requestOptions = {
-      method: 'PATCH',
+      method: 'POST',
       headers: myHeaders,
-      body: urlEncoded,
+      body: formData,
       redirect: 'follow'
     };
 
     await fetch(`${apiBaseUrl}/services/${serviceToUpdate.id}`, requestOptions)
-    .then((response) => {
+    .then(async (response) => {
       if (response.ok) {
-        serviceName.innerHTML = formDataName;
-        serviceDescription.innerHTML = formDataDescription;
+        const updatedService = await response.json();
+        const updatedServiceIndex = servicesArray.findIndex((service) => service.id === updatedService.id);
+        servicesArray[updatedServiceIndex] = updatedService;
+        console.log(servicesArray[updatedServiceIndex]);
+        
+        displayAllServices();
         serviceUpdateModal.hide();
       } else {
         alert("Vos modifications n'ont pas pu être sauvegardées.");
@@ -107,38 +97,60 @@ const updateService = async () => {
 serviceDeleteModalEl.addEventListener('show.bs.modal', (event) => {
   const button = event.relatedTarget;
   const serviceId = button.getAttribute('data-bs-service-id');
-  const serviceToUpdate = servicesArray.find((service) => (service.id === Number(serviceId)));
-  serviceDeleteWarning.innerHTML = `Vous vous apprêtez à supprimer le service "${serviceToUpdate.name}".`
+  serviceToDelete = servicesArray.find((service) => (service.id === Number(serviceId)));
+  serviceDeleteWarning.innerHTML = `Vous vous apprêtez à supprimer le service "${serviceToDelete.name}".`
   serviceDeleteButton.addEventListener('click', deleteService);
 })
 
-const deleteService = async () => {}
-
-const createService = async () => {
-  const formData = new FormData(serviceCreateForm);
-  const formDataName = formData.get('name');
-  const formDataDescription = formData.get('description');
+const deleteService = async () => {
   const token = getConnexionToken();
 
   const myHeaders = new Headers();
-  myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
-   myHeaders.append('Authorization', `Bearer ${token}`);
+  myHeaders.append('Authorization', `Bearer ${token}`);
 
-  const urlEncoded = new URLSearchParams();
-  urlEncoded.append('name', formDataName);
-  urlEncoded.append('description', formDataDescription);
+  const requestOptions = {
+    method: 'DELETE',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  await fetch(`${apiBaseUrl}/services/${serviceToDelete.id}`, requestOptions)
+  .then((response) => {
+    if (response.ok) {
+      servicesArray.forEach((service, index) => {
+        if (service.id === serviceToDelete.id) {
+          servicesArray.splice(index, 1);
+        }
+      });
+      displayAllServices();
+      serviceDeleteModal.hide();
+    } else {
+      alert(`Le service "${serviceToDelete.name}" n'a pas pu être supprimé.`);
+    }
+  })
+  .catch((error) => console.error(error));
+}
+
+const createService = async () => {
+  const formData = new FormData(serviceCreateForm);
+  const token = getConnexionToken();
+
+  const myHeaders = new Headers();
+  myHeaders.append('Authorization', `Bearer ${token}`);
 
   const requestOptions = {
     method: 'POST',
     headers: myHeaders,
-    body: urlEncoded,
+    body: formData,
     redirect: 'follow'
   };
 
-  await fetch(`${apiBaseUrl}/services`, requestOptions)
-    .then((response) => {
+    await fetch(`${apiBaseUrl}/services`, requestOptions)
+    .then(async (response) => {
       if (response.ok) {
-        servicesArray.push(response);
+        const newService = await response.json();
+
+        servicesArray.push(newService);
         displayAllServices();
         serviceCreateModal.hide();
       } else {
