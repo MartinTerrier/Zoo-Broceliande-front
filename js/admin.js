c9dac6fd7ec91e6d891ef669ebb5bef8540bbf6e
Martin Terrier <martin.terrier@gmail.com>
2024-10-12 16:43:12 +0200

    Animal creation functional

--- js/admin.js
+++ js/admin.js
@@ -9,6 +9,15 @@ const habitatUpdateNameInput = document.getElementById('habitat-update-name-inpu
 const habitatUpdateContentInput = document.getElementById('habitat-update-content-input');
 const habitatUpdateImageInput = document.getElementById('habitat-update-image-input');
 
+const animalCreateModalEl = document.getElementById('create-animal-modal');
+const animalCreateModal = new bootstrap.Modal(animalCreateModalEl);
+const animalCreateForm = document.getElementById('animal-create-form');
+const animalCreateNameInput = document.getElementById('animal-create-name-input');
+const animalCreateSpeciesInput = document.getElementById('animal-create-species-input');
+const animalCreateHabitatInput = document.getElementById('animal-create-habitat-input');
+const animalCreateImageInput = document.getElementById('animal-create-image-input');
+const animalCreateButton = document.getElementById('animal-create-button');
+
 const preloadSelectedHabitat = async (habitatId) => {
     habitatUpdateConfirmation.innerHTML = '';
     const token = getConnexionToken();
@@ -69,3 +78,53 @@ const updateHabitat = async () => {
 }
 
 habitatUpdateButton.addEventListener('click', updateHabitat);
+
+const getSpeciesSelectHtml = async () => {
+  const speciesArray = await fetch(`${apiBaseUrl}/animals/species`, { method: "GET", redirect: "follow"})
+  .then((response) => (response.json()))
+  .catch((error) => console.error(error));
+  
+  let speciesSelectHtml = '';
+  speciesArray.forEach((species) => {
+    speciesSelectHtml += `<option value="${species.id}">${species.label}</option>`
+  });
+  return speciesSelectHtml;
+}
+
+animalCreateModalEl.addEventListener('show.bs.modal', async () => {
+  animalCreateNameInput.value = null;
+  animalCreateSpeciesInput.innerHTML = await getSpeciesSelectHtml();
+  animalCreateImageInput.value = null;
+  animalCreateButton.addEventListener('click', createAnimal);
+});
+  
+  const createAnimal = async () => {
+    const formData = new FormData(animalCreateForm);
+    formData.speciesId = +formData.habitatId;
+    formData.habitatId = +formData.habitatId;
+    
+    const token = getConnexionToken();
+  
+    const myHeaders = new Headers();
+    myHeaders.append('Authorization', `Bearer ${token}`);
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: formData,
+      redirect: 'follow'
+    };
+  
+      await fetch(`${apiBaseUrl}/animals`, requestOptions)
+      .then(async (response) => {
+        if (response.ok) {
+          const newAnimal = await response.json();
+          console.log(newAnimal);
+  
+          animalCreateModal.hide();
+        } else {
+          alert("Le nouveau service n'a pas pu être créé.");
+        }
+      })
+      .catch((error) => console.error(error));
+  }
