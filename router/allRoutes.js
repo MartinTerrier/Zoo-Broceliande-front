d96cdbea2ae09a0d1c4b881e6bc662c5f6ae0dc4
Martin Terrier <martin.terrier@gmail.com>
2024-10-14 23:21:46 +0200

    Deploy test

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from './route.js';
 export const allRoutes = [
     new Route('/', 'Accueil', [], '/pages/home.html'),
     new Route('/signin', 'Connexion', [ 'disconnected' ], '/pages/signin.html', '/js/auth/signin.js'),
-    new Route('/services', 'Nos services', [], '/pages/services.html', '/js/services.js'),
+    new Route('/services', 'Nos services', [], 'pages/services.html', '/js/services.js'),
     new Route('/habitats', 'Habitats et animaux', [], '/pages/habitats.html'),
     new Route('/comments', 'Vos commentaires', [], '/pages/comments.html', '/js/comments.js'),
     new Route('/contact', 'Contactez-nous', [], '/pages/contact.html', '/js/contact.js'),
